424b578c02427a9a3d36b24f9e0687c9ba975ae2
Nick Newbill <nick.newbill@sas.com>
2026-07-22 16:32:37 -0400

    update for global clipboard

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.opt.clipboard = "unnamedplus"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
